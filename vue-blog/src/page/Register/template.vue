b1f546eff7920ed8bf70d04ea6707bd842a31efa
Composur <xutongxw@163.com>
2018-05-29 11:17:34 +0800

    add router

---
+++ vue-blog/src/page/Register/template.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script src='./template.js'></script>
+
+<style scoped src='./template.css'></style>
+
+
--- vue-blog/src/router/index.js
+++ vue-blog/src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/page/Login/template.vue'
+import Register from '@/page/Register/template.vue'
+import Index from '@/page/Index/template.vue'
+import Edit from '@/page/Edit/template.vue'
+import Detail from '@/page/Detail/template.vue'
+import Create from '@/page/Create/template.vue'
 
 Vue.use(Router)
 
@@ -9,7 +14,37 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: Login
+    },
+    {
+      path: '/login',
+      name: 'HelloWorld',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'test',
+      component: Register
+    },
+    {
+      path: '/index',
+      name: 'test',
+      component: Index
+    },
+    {
+      path: '/edit',
+      name: 'test',
+      component: Edit
+    },
+    {
+      path: '/detail',
+      name: 'test',
+      component: Detail
+    },
+    {
+      path: '/create',
+      name: 'test',
+      component: Create
     }
   ]
 })
