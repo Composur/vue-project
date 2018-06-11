28ec2647a1ab87804a953179088b3d74d4c9bb32
Composur <xutongxw@163.com>
2018-06-11 15:47:54 +0800

    create page

---
+++ vue-blog/src/page/Create/create.js
@@ -0,0 +1,15 @@
+export default {
+    data() {
+      return {
+        title: '',
+        content: '',
+        description:'',
+        atIndex:false
+      }
+    },
+    methods:{
+        onCreate() {
+            
+        }
+    }
+  };
--- vue-blog/src/page/Create/create.vue
+++ vue-blog/src/page/Create/create.vue
@@ -2,15 +2,23 @@
   <div id="edit">
     <h1>创建文章</h1>
     <h3>文章标题</h3>
-    <el-input></el-input>
+    <el-input v-model="title"></el-input>
     <p class="msg">限30个字</p>
     <h3>内容简介</h3>
-    <el-input type="textarea" rows=3></el-input>
+    <el-input type="textarea" v-model="content" autosize></el-input>
     <p class="msg">限30个字</p>
     <h3>文章内容</h3>
-    <el-input type="textarea" rows=20></el-input>
+    <el-input type="textarea"  v-model="description"  :autosize="{ minRows: 8, maxRows: 15}"></el-input>
     <p class="msg">限30个字</p>
-    <el-button >确定</el-button>
+    <p>
+      是否展示到首页
+    <el-switch
+      v-model="atIndex"
+      active-color="#13ce66"
+      inactive-color="#ff4949">
+    </el-switch>
+   </p>
+    <el-button v-on:click='onCreate()'>确定</el-button>
   </div>
 </template>
 
