3afbeb23b94758854e530dc1bfe0948cbb79b1cd
Composur <xutong1107@gmail.com>
2019-11-24 21:28:31 +0800

    init:vue-guide

--- vue-blog/src/page/Detail/detail.js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: 'test',
-  data () {
-    return {
-      msg: 'detail!'
-    }
-  }
-}
--- vue-blog/src/store/module/blog.js
+++
@@ -1,6 +0,0 @@
-export default {
-    state:{},
-    getter:{},
-    mutations:{},
-    actions:{}
-}
