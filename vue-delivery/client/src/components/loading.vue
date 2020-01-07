d7e733d9890b262930ff4257b680cf965f23a8ec
Composur <xutong1107@gmail.com>
2020-01-07 20:10:53 +0800

    fix:loading

---
+++ vue-delivery/client/src/components/loading.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="mask">
+      <mt-spinner  type="fading-circle"  :size="60"></mt-spinner>
+  </div>
+</template>
+<script>
+import Vue from 'vue'
+import { Spinner } from 'mint-ui';
+Vue.component(Spinner.name, Spinner);
+export default {
+ 
+}
+</script>
+
+<style>
+.mask {
+  position: absolute;
+  z-index: 9999;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.6);
+}
+.mint-spinner-fading-circle{
+  position: absolute;
+  margin: auto;
+  left: 0;
+  bottom: 0;
+  top: 0;
+  right: 0;
+  width:60px;
+  height:60px
+}
+</style>
+
