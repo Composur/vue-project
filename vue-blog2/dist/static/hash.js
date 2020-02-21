31947dee6510ae9e99759dd49262267b3b257ee2
Composur <xutong1107@gmail.com>
2020-02-21 23:22:00 +0800

    build:切片上传

---
+++ vue-blog2/dist/static/hash.js
@@ -0,0 +1,33 @@
+self.importScripts("./spark-md5.min.js"); // 导入脚本
+
+// 生成文件 hash
+self.onmessage = e => {
+  const { fileChunkList } = e.data;
+  const spark = new self.SparkMD5.ArrayBuffer();
+  let percentage = 0;
+  let count = 0;
+  const loadNext = index => {
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(fileChunkList[index].file);
+    reader.onload = e => {
+      count++;
+      spark.append(e.target.result);
+      // 完毕后报告给 UI 线程
+      if (count === fileChunkList.length) {
+        self.postMessage({
+          percentage: 100,
+          hash: spark.end()
+        });
+        self.close();
+      } else {
+        percentage += 100 / fileChunkList.length;
+        self.postMessage({
+          percentage
+        });
+        // 递归
+        loadNext(count);
+      }
+    };
+  };
+  loadNext(0);
+};
